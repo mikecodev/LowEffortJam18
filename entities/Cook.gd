f835b4ad6ce4a738c582d663c5eeac622b3f1441
jairo <jairo@agentero.com>
2022-01-16 22:58:37 +0100

    up

--- entities/Cook.gd
+++ entities/Cook.gd
@@ -4,14 +4,16 @@ class_name Cook
 
 const c_pizza = preload("res://entities/Pizza.tscn")
 
-func add_pizza(order):
-	var pizza = c_pizza.instance()
-	pizza.SetPizzaType(order)
-	get_parent().add_child(pizza)
-	pizza.global_position = Vector2(global_position.x, global_position.y + 12)
+remotesync func add_pizza(order):
+	if Net.is_from_server():
+		var pizza = c_pizza.instance()
+		pizza.SetPizzaType(order)
+		get_parent().add_child(pizza)
+		pizza.global_position = Vector2(global_position.x, global_position.y + 12)
 
 func check_orders():
-	var areas = $Area2D.get_overlapping_areas()
-	if Defs.orders.size() > 0 and areas.size() == 0:
-		var order = Defs.orders.pop_front()
-		add_pizza(order)
+	if is_network_master():
+		var areas = $Area2D.get_overlapping_areas()
+		if Defs.orders.size() > 0 and areas.size() == 0:
+			var order = Defs.orders.pop_front()
+			rpc("add_pizza", order)
