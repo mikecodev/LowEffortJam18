bf3d1906886befa5f55af8086d68c70e4b3aae01
mikecodev <codevmike@gmail.com>
2022-01-16 22:39:39 +0100

    mec

--- entities/Cook.gd
+++ entities/Cook.gd
@@ -4,16 +4,15 @@ class_name Cook
 
 const c_pizza = preload("res://entities/Pizza.tscn")
 
-var served: bool = false
-
 func add_pizza(order):
-	served = true
 	var pizza = c_pizza.instance()
 	pizza.SetPizzaType(order)
 	get_parent().add_child(pizza)
 	pizza.global_position = Vector2(global_position.x, global_position.y + 12)
 
 func check_orders():
-	if Defs.orders.size() > 0 and !served:
+	var areas = $Area2D.get_overlapping_areas()
+	print(areas)
+	if Defs.orders.size() > 0 and areas.size() == 0:
 		var order = Defs.orders.pop_front()
 		add_pizza(order)
